8d77595ee259be8cca736a0139e46d7085d48d56
sharon-512 <89990973+sharon-512@users.noreply.github.com>
2021-09-02 22:03:13 +0530

    Add files via upload

---
+++ BankingApp/README.md
@@ -0,0 +1,3 @@
+#BasicBankingApp
+
+Basic Banking System website. A web application used to transfer virtual money between multiple users and also record the banking transactions/ activities.
---
+++ BankingApp/the-sparks-foundation-main/navbar.php
@@ -0,0 +1,47 @@
+<html>
+<head>
+<style>
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #ff4d4d;
+}
+
+li {
+  float: left;
+}
+
+li a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 20px 20px;
+  text-decoration: none;
+}
+
+li a:hover:not(.active) {
+  background-color: #e60000;
+  text-decoration: none;
+  color: white;
+}
+
+.active {
+  background-color: black;
+}
+</style>
+</head>
+<body>
+
+<ul>
+  <li><a style="background-color: black; color: white;">SPARK BANK   &nbsp &nbsp &nbsp &nbsp &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp </a></li>
+  <li><a href="index.php">HOME</a></li>
+  <li><a href="createuser.php">CREATE USER</a></li>
+  <li><a href="transfermoney.php">TRANSFER MONEY</a></li>
+  <li><a href="transactionhistory.php">TRANSACTION HISTORY</a></li>
+</ul>
+
+</body>
+</html>
+         
